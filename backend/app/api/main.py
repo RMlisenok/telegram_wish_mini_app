bf168c53b144ac0f740da304076c791f02317e53
Kikos1337 <skalisusov@inbox.ru>
2025-12-03 12:33:30 +0300

    fix problem

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,23 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.core.db import init_database, drop_tables
-from app.api.routers.auth import router as auth_routers
 
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_database()
-    yield
-    print('Stop work and clean tables')
-    await drop_tables()
-    print('clean completed')
-
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(auth_routers)
-
-
-@app.get('/')
+@app.get("/")
 async def root():
-    return {'message': 'Backend Telegram mini app work'}
+    return {"message": "Hello World"}
