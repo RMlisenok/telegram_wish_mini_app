7f10368bbbca648b74a6f8f1621aac5820d54e59
Kikos1337 <skalisusov@inbox.ru>
2025-12-15 05:13:41 +0300

    create wishlist model

---
+++ backend/app/models/wishlist.py
@@ -0,0 +1,55 @@
+from typing import Optional, List
+from sqlalchemy import String, Enum, Text, TIMESTAMP, BigInteger, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import enum
+
+from app.core.base import Base
+
+
+class TypePrivacyEnum(enum.Enum):
+    public = 'public'
+    private = 'private'
+    protected = 'protected'
+
+
+class User(Base):
+    __tablename__ = 'wishlists'
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    user_id = Mapped[int] = mapped_column(
+        BigInteger,
+        ForeignKey('users.id'),
+        nullable=False
+    )
+    name: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
+    description: Mapped[str] = mapped_column(
+        String(250),
+        nullable=True
+    )
+    photo: Mapped[Optional[str]] = mapped_column(
+        Text,
+        nullable=True
+    )
+    typeprivacy: Mapped[TypePrivacyEnum] = mapped_column(
+        Enum(TypePrivacyEnum),
+        default=TypePrivacyEnum.public
+    )
+    created_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now()
+    )
+    updated_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+
+    owner: Mapped['User'] = relationship(
+        back_populates='wishlists'
+    )
