cb7d4bb2f3d273e3f7b06f95fc949b979fcaa7e1
Kikos1337 <skalisusov@inbox.ru>
2025-12-16 05:00:24 +0300

    create wish schema v_1

---
+++ backend/app/schemas/wish.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional, List
+from datetime import date, datetime
+from enum import Enum
+
+
+class CurrencyEnum(str, Enum):
+    RUB = "RUB"
+    BYN = "BYN"
+    USD = "USD"
+    EUR = "EUR"
+    UAH = "UAH"
+    KZT = "KZT"
+
+
+class WishBase(BaseModel):
+    user_id: int
+    name: str
+    photo: Optional[str] = None
+    usrl_gift: str
+    price: float
+    currency: CurrencyEnum = CurrencyEnum.RUB
+    description: str
+
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class WishCreate(WishBase):
+    pass
+
+
+class WishUpdate(BaseModel):
+    name: str
+    photo: Optional[str] = None
+    usrl_gift: str
+    price: float
+    currency: CurrencyEnum = CurrencyEnum.RUB
+    description: str
+
+
+class WishResponse(WishBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    wishlists: List["WishlistShort"] = []
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class WishShort(BaseModel):
+    id: int
+    name: str
+    photo: Optional[str] = None
+    url_gift: str
+    currency: CurrencyEnum
+    price: int
+    is_booked: bool
+
+    model_config = ConfigDict(from_attributes=True)
