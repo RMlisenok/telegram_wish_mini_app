bd63c660164724805b08f994156c83c9f9e797f9
Kikos1337 <skalisusov@inbox.ru>
2025-12-19 01:01:26 +0300

    create wish_reservation reporitory

---
+++ backend/app/repositories/wish_reservation_repository.py
@@ -0,0 +1,84 @@
+from typing import Optional, List
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+from app.models.wish_reservation import WishReservation
+from app.models.wish_wishlist import WishWishlist
+
+
+class WishReservationRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get(
+        self,
+        wish_reservation_id: int
+    ) -> Optional[WishReservation]:
+        query = select(WishReservation).where(
+            WishReservation.id == wish_reservation_id
+        )
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_wish(
+        self,
+        wish_wishlist_id: int
+    ) -> Optional[WishReservation]:
+        query = select(WishReservation).where(
+            WishReservation.wish_wishlist_id == wish_wishlist_id
+        )
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_user_reservations(
+        self,
+        reserved_id: int,
+        limit: int = 10
+    ) -> List[WishReservation]:
+        query = select(WishReservation).where(
+            WishReservation.reserved_by_id == reserved_id
+        ).limit(limit).options(
+            selectinload(WishReservation.wish_wishlist).selectinload(WishWishlist.wish),
+            selectinload(WishReservation.wish_wishlist).selectinload(WishWishlist.wishlist)
+        )
+        result = await self.session.execute(query)
+        return list(result.scalars().all())
+
+    async def check_wish_reservation(
+        self,
+        wish_wihslist_id: int
+    ) -> bool:
+        query = select(WishReservation).where(
+            WishReservation.wish_wishlist_id == wish_wihslist_id)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none is not None
+
+    async def create(
+        self,
+        wish_wishlist_id: int,
+        reserved_by_id: int
+    ) -> Optional[WishReservation]:
+        existing = self.check_wish_reservation(wish_wishlist_id)
+        if existing:
+            return None
+        reservation = WishReservation(
+            wish_wishlist_id=wish_wishlist_id,
+            reserved_by_id=reserved_by_id
+        )
+        self.session.add(reservation)
+        await self.session.commit()
+        await self.session.refresh(reservation)
+        return reservation
+
+    async def delete(
+        self,
+        wish_wishlist_id: int,
+        reserved_by_id: int
+    ) -> bool:
+        reservation = self.get(wish_wishlist_id, reserved_by_id)
+        if reservation:
+            await self.session.delete(reservation)
+            await self.session.commit()
+            return True
+        return False
