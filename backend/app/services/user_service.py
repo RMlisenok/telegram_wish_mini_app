549102eb5c2cff268c798b08f7e83b91b4dba4a9
Kikos1337 <skalisusov@inbox.ru>
2025-12-19 01:43:20 +0300

    short name user_repository

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,21 +3,22 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repositories.user_repository import UserRepositories
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserResponse
 
 
 class UserService:
     def __init__(self, session: AsyncSession):
-        self.repository = UserRepositories(session)
+        self.rep_user = UserRepositories(session)
 
     async def get_user(self, user_id: int) -> Optional[User]:
-        return await self.repository.get_user_by_id(user_id)
+        return await self.rep_user.get_user_by_id(user_id)
 
     async def get_user_by_telegram_id(
         self,
         telegram_id: int
     ) -> Optional[User]:
-        return await self.repository.get_user_by_tg_id(telegram_id)
+        return await self.rep_user.get_user_by_tg_id(telegram_id)
 
     async def create_user(self, user_data: UserCreate) -> User:
-        return await self.repository.create(user_data)
+        user = await self.rep_user.create(user_data)
+        return UserResponse.model_validate(user)
