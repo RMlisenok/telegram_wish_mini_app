209cceafad0a4e8268671f398083b31570c294d4
Kikos1337 <skalisusov@inbox.ru>
2025-12-22 00:55:46 +0300

    add logic block, unblock and get

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -2,13 +2,16 @@ from typing import Optional, List
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repositories.user_repository import UserRepository
+from app.repositories.block_repository import BlockRepository
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse, UserUpdate
+from app.schemas.block import BlockCreate, BlockListResponse, BlockResponse, BlockStatusResponse
 
 
 class UserService:
     def __init__(self, session: AsyncSession):
         self.rep_user = UserRepository(session)
+        self.rep_block = BlockRepository(session)
 
     async def get_user(self, user_id: int) -> Optional[User]:
         return await self.rep_user.get_user_by_id(user_id)
@@ -35,4 +38,38 @@ class UserService:
         user_id: int,
         user_data: UserUpdate
     ) -> Optional[UserResponse]:
-        pass
+        user = await self.update_user(user_id, user_data)
+        if user:
+            return UserResponse.model_validate(user)
+        return None
+
+    async def block_user(
+        self,
+        blocker_id: int,
+        blocked_id: int,
+    ) -> Optional[BlockResponse]:
+        block = await self.rep_block.block_user(blocker_id, blocked_id)
+        if block:
+            return BlockResponse.model_validate(block)
+        return None
+
+    async def unblock_user(
+        self,
+        blocker_id: int,
+        blocked_id: int,
+    ) -> bool:
+        return await self.rep_block.unblock_user(blocker_id, blocked_id)
+
+    async def check_block_status(
+        self,
+        blocker_id: int,
+        blocked_id: int,
+    ) -> bool:
+        return await self.rep_block.is_user_blocked(blocker_id, blocked_id)
+
+    async def get_user_block(
+        self,
+        blocker_id: int
+    ) -> List[UserResponse]:
+        users = await self.rep_block.get_user_block(blocker_id)
+        return [UserResponse.model_validate(user) for user in users]
