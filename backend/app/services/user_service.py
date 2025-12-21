4d7ac7cb47d7ab27042a3a0f891470fe334c28f8
Kikos1337 <skalisusov@inbox.ru>
2025-12-22 00:23:43 +0300

    add update function and get all users

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, List
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repositories.user_repository import UserRepository
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.user import UserCreate, UserResponse, UserUpdate
 
 
 class UserService:
@@ -19,6 +19,20 @@ class UserService:
     ) -> Optional[User]:
         return await self.rep_user.get_user_by_tg_id(telegram_id)
 
+    async def get_all_users(
+        self,
+        limit: int = 10
+    ) -> List[UserResponse]:
+        users = await self.get_all_users(limit)
+        return [UserResponse.model_validate(user) for user in users]
+
     async def create_user(self, user_data: UserCreate) -> User:
         user = await self.rep_user.create(user_data)
         return UserResponse.model_validate(user)
+
+    async def update_user(
+        self,
+        user_id: int,
+        user_data: UserUpdate
+    ) -> Optional[UserResponse]:
+        pass
