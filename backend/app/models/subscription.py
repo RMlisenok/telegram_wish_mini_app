e37e1046e631a6c155a277f7f53ec1438da3899b
Kikos1337 <skalisusov@inbox.ru>
2025-12-23 22:18:58 +0300

    create model subscription

---
+++ backend/app/models/subscription.py
@@ -0,0 +1,60 @@
+from typing import Optional
+from sqlalchemy import Boolean, TIMESTAMP, BigInteger, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.base import Base
+
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True
+    )
+
+    subscriber_id: Mapped[int] = mapped_column(
+        BigInteger,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True
+    )
+
+    type_sub: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=True
+    )
+    target_user_id: Mapped[int] = mapped_column(
+        BigInteger,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=True,
+        index=True
+    )
+    target_wishlist_id: Mapped[int] = mapped_column(
+        BigInteger,
+        ForeignKey("wishlists.id", ondelete="CASCADE"),
+        nullable=True,
+        index=True
+    )
+    created_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now()
+    )
+
+    subscriber: Mapped[Optional["User"]] = relationship(
+        "Users",
+        foreign_keys=[subscriber_id],
+        back_populates="my_subscriptions"
+    )
+    target_user: Mapped[Optional["User"]] = relationship(
+        "Users",
+        foreign_keys=[target_user_id],
+        back_populates="subscribers_to_me"
+    )
+    target_wishlist: Mapped[Optional["Wishlist"]] = relationship(
+        "Wishlist",
+        foreign_keys=[target_wishlist_id],
+        back_populates="subscribers"
+    )
