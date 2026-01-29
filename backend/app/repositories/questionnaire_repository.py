f9c4016fb69348389499ecbad59f77df39340b84
Kikos1337 <skalisusov@inbox.ru>
2026-01-29 16:44:48 +0300

    create repository

---
+++ backend/app/repositories/questionnaire_repository.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.questionnaire import UserForm, TagForm
+
+
+class QuestionnaireRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_tags_by_type(self, is_interest: bool):
+        query = select(TagForm).where(TagForm.type_tags == is_interest)
+        result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def save_user_forms(self, user_id: int, items: list[UserForm]):
+        # Сначала очищаем старую анкету, затем пишем новую
+        await self.session.execute(delete(UserForm).where(UserForm.user_id == user_id))
+        self.session.add_all(items)
+        await self.session.commit()
