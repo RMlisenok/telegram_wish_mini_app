385c712fcbf120195b02aa702899d7465df8d21a
Dass254 <dvagina@inbox.ru>
2026-02-05 15:12:13 +0300

    check console log total subscribers

--- frontend/src/types/mainScreenData.ts
+++ frontend/src/types/mainScreenData.ts
@@ -118,6 +118,7 @@ export async function loadMainScreenData(token: string) {
             birth_date: formatDateToDDMMYYYY(sub.birth_date)
         }));
         mainSubscribersStore.set(transformedSubscribers);
+        console.log(data.subscribers.total);
 
         totalWishesStore.set(data.total_wish || 0);
         totalWishlistsStore.set(data.total_wishlist || 0);
