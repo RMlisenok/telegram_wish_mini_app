0ce03118aaa5f5fbd72c8d5d4dc3744702fdc114
Kikos1337 <skalisusov@inbox.ru>
2026-04-06 13:41:42 +0300

    add recommendations schems

---
+++ backend/app/schemas/recommendations.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class GiftResponse(BaseModel):
+    title: str
+    description: str
+    url: str
+    category: str
+
+    class Config:
+        from_attributes = True
