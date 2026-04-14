09e3bffacf3c5508ac287e960928ccb236e61633
konstable98 <161381991+Konstanta202@users.noreply.github.com>
2026-04-14 11:59:06 +0300

    fix function data_tags.py

--- backend/app/core/data_tags.py
+++ backend/app/core/data_tags.py
@@ -25,24 +25,6 @@ AVOID_TAGS = [
 async def init_tags():
     async with AsyncSession(bind=async_engine) as session:
         try:
-<<<<<<< HEAD
-            check = await session.execute(select(TagForm).limit(1))
-            if check.scalars().first():
-                return
-    
-            for t in INTEREST_TAGS:
-                session.add(TagForm(tag_value=t, type_tags=True))
-    
-            for t in AVOID_TAGS:
-                session.add(TagForm(tag_value=t, type_tags=False))
-    
-            await session.commit()
-            print("✅ Теги успешно инициализированы")
-    
-        except Exception as e:
-            await session.rollback()
-            print(f'❌ ERROR в init_tags - {e}')
-=======
             async with session.begin():
                 check = await session.execute(select(TagForm).limit(1))
                 if check.scalars().first():
@@ -57,5 +39,4 @@ async def init_tags():
                 await session.commit()
         except Exception as e:
             print(f'ERRROOOR - {e}')
->>>>>>> backend
             raise
