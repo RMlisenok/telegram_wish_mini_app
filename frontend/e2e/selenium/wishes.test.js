50d8d9af3f4f7b34202d4a5705842805f36c5795
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2026-04-28 01:07:43 +0300

    e2e test: creating wish with required name

---
+++ frontend/e2e/selenium/wishes.test.js
@@ -0,0 +1,25 @@
+import { clickByAnyText, expectText, fillFirstMatchingInput, openApp } from './helpers.js';
+import { defineScenarioSuite } from './scenarioRunner.js';
+
+async function openCreateWish(driver, By, until, baseUrl) {
+  await openApp(driver, baseUrl);
+  await clickByAnyText(driver, By, until, ['Желания', 'Ваши желания']);
+  await clickByAnyText(driver, By, until, ['Создать желание', 'Добавить желание', '+ Новое желание', 'Новое желание']);
+}
+
+defineScenarioSuite('Wishes E2E scenarios', [
+  {
+    number: 17,
+    title: 'Создание желания с обязательным названием',
+    requirements: ['FS-6.1', 'FS-6.2', 'NFS-6.1'],
+    requires: ['authenticated'],
+    run: async ({ driver, By, until, baseUrl }) => {
+      await openCreateWish(driver, By, until, baseUrl);
+      await clickByAnyText(driver, By, until, ['Сохранить']);
+      await expectText(driver, By, until, ['Название', 'обязательно']);
+      await fillFirstMatchingInput(driver, By, 'E2E wish', ['input']);
+      await clickByAnyText(driver, By, until, ['Сохранить']);
+      await expectText(driver, By, until, 'E2E wish');
+    },
+  },
+]);
